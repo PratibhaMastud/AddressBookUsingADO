c7d6be17b7c8a4b0e2207f47b427b9248c94bd5d
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-30 19:53:19 +0530

    [Pratibha Mastud] Add : Connection Established Or Not

---
+++ AddressBookADO/AddressBookADO/AddressRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookADO
+{
+    public class AddressRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocaldb;Initial Catalog=AddressBook;Integrated Security=True";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+        public void CheckConnection()
+        {
+            try
+            {
+                this.sqlConnection.Open();
+                Console.WriteLine("Connection Established");
+                this.sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ AddressBookADO/AddressBookADO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBookADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in Address Book ADO");
+            AddressRepo Repo = new AddressRepo();
+            Repo.CheckConnection();
+        }
+    }
+}
